837e0da69feed0d1a2bf22df1397d8f6904d243b
kaysen <632393819@qq.com>
2022-04-24 12:27:07 +0800

    Hot-fix to Commit02 --kaysen

--- src/main/java/com/atChina/gitTest/gitDemo01.java
+++ src/main/java/com/atChina/gitTest/gitDemo01.java
@@ -4,17 +4,18 @@ public class gitDemo01 {
 
     public static void main(String[] args) {
 
+        //在hot-fix阶段增加注释  第一次提交
         System.out.println("Frist Commit");
         System.out.println("git.test02222222");
         System.out.println("git.test02222222");
         System.out.println("git.test02222222");
 
-
+        //在hot-fix阶段增加注释  这是第二次提交
         System.out.println("git.test03333333");
         System.out.println("git.test03333333");
         System.out.println("git.test03333333");
 
-
+        //在hot-fix阶段增加注释   这是第三次提交  hot-fix提交
         System.out.println("hot-fix test011111");
         System.out.println("hot-fix test011111");
         System.out.println("hot-fix test011111");
